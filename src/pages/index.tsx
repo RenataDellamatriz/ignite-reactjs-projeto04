a4761540ecac62165b5f867147705ae883db5522
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-04-26 16:28:35 -0300

    global styles, stitches theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,13 @@
+import { styled } from "../styles";
+
+const Button = styled('button', {
+  backgroundColor: '$green300',
+
+  '&:hover': {
+    backgroundColor: "White"
+  }
+})
+
 export default function Home() {
-  return <><h1>Hello world</h1></>;
+  return <><Button>Enviar</Button></>;
 }
