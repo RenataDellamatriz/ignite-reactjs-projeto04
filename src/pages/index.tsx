22c07f85c080861f40ff0978d1a2b4282f053e55
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-04-29 19:38:55 -0300

    feat: home style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
+import { HomeContainer, Product } from "@/styles/pages/home";
 import { styled } from "../styles";
+import Image from "next/image";
+import camiseta1 from '../assets/camisetas/Shirt-1.png'
+import camiseta2 from '../assets/camisetas/Shirt-2.png'
+import camiseta3 from '../assets/camisetas/Shirt-3.png'
+
 
 const Button = styled('button', {
   backgroundColor: '$green300',
@@ -9,5 +15,28 @@ const Button = styled('button', {
 })
 
 export default function Home() {
-  return <><Button>Enviar</Button></>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image alt="" src={camiseta1}/>
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image alt="" src={camiseta2}/>
+        <footer>
+          <strong>Camiseta y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>   <Product>
+        <Image alt="" src={camiseta2}/>
+        <footer>
+          <strong>Camiseta z</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
