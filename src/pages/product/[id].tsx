a55375048f824fe076ca999fe4fb142c301f2abe
Renata Dellamatriz <r.dellamatriz@hotmail.com>
2023-05-14 12:40:14 -0300

    feat: success page, product page, checkout handler

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -8,6 +8,8 @@ import { GetStaticPaths, GetStaticProps } from "next";
 import { stripe } from "@/lib/stripe";
 import Stripe from "stripe";
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
 
 interface ProductProps {
   product: {
@@ -15,15 +17,41 @@ interface ProductProps {
     name: string;
     imageUrl: string;
     price: string;
-    description: string
+    description: string;
+    defaultPriceId: string;
   };
 }
 
-export default function Product({product}: ProductProps) {
-  const {isFallback} = useRouter()
+export default function Product({ product }: ProductProps) {
+  // const { isFallback } = useRouter();
 
-  if ( isFallback) {
-    return <p>loading...</p>
+  // if (isFallback) {
+  //   return <p>loading...</p>;
+  // }
+
+  // const router = useRouter()
+
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false);
+
+  async function handleByProduct() {
+    try {
+      setIsCreatingCheckoutSession(true);
+      const response = await axios.post("/api/checkout", {
+        priceId: product.defaultPriceId,
+      });
+
+      const { checkoutUrl } = response.data;
+
+      // router.push('/checkout') para redirecionar internamente
+
+      window.location.href = checkoutUrl; // redirecionar externo
+    } catch (error) {
+      // Conectar com uma ferramenta de observabilidade
+      // (Datadog / Sentry)
+      setIsCreatingCheckoutSession(false);
+      alert("Falha ao redirecionar ao checkout");
+    }
   }
 
   return (
@@ -36,11 +64,9 @@ export default function Product({product}: ProductProps) {
         <h1>{product.name}</h1>
         <span>{product.price}</span>
 
-        <p>
-         {product.description}
-        </p>
+        <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleByProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
@@ -49,11 +75,11 @@ export default function Product({product}: ProductProps) {
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
     paths: [
-      {params: { id: 'prod_Nof8bK9gBTxKE9'}} // colocar só o essencial ou nada -> []
+      { params: { id: "prod_Nof8bK9gBTxKE9" } }, // colocar só o essencial ou nada -> []
     ],
-    fallback: true
-  }
-}
+    fallback: true,
+  };
+};
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
   params,
@@ -83,7 +109,8 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
         name: product.name,
         imageUrl: product.images[0],
         price: formattedPrice,
-        description: product.description
+        description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, //1 hour
